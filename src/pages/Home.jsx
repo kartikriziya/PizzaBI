424c033cee49aea6959e7f9da6435d7d3f6289b9
Kartik Riziya <kartikriziya30721@gmail.com>
2026-05-24 16:02:16 +0200

    Frontend Setup (React + Tailwind)
    Initial Commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,9 @@
+function Home() {
+  return (
+    <div className="min-h-screen flex items-center justify-center text-3xl font-bold">
+      React + Tailwind Setup Complete 🚀
+    </div>
+  )
+}
+
+export default Home
