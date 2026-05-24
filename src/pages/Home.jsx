603e1753b949757979475b0d8e0d1143ff6183d8
amirismail-uas <amir.ismail@stud.fra-uas.de>
2026-05-24 16:28:34 +0200

    First commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,7 @@
 function Home() {
   return (
     <div className="min-h-screen flex items-center justify-center text-3xl font-bold">
-      React + Tailwind Setup Complete 🚀
+      React + Tailwind Setup Complete Amir 🚀
     </div>
   )
 }
